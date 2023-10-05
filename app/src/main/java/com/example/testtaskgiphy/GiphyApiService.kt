3f5f4d14c9d12f75774333dcad291d18a2e1e3d4
milkyway <i.am.molokova@gmail.com>
2023-10-06 02:15:07 +0300

    updating apiservice

--- app/src/main/java/com/example/testtaskgiphy/GiphyApiService.kt
+++ app/src/main/java/com/example/testtaskgiphy/GiphyApiService.kt
@@ -1,9 +1,18 @@
 package com.example.testtaskgiphy
 
+import retrofit2.Response
+import retrofit2.http.GET
+import retrofit2.http.Query
+
 interface GiphyApiService {
-
-    @GET("gifs/random")
-    suspend fun getTrendingGifs(): Response<GiphyResponse>
-
+    @GET("gifs/random") //или @
+    suspend fun getRandomGifs(
+        @Query("api_key") apiKey: String,
+        @Query("q") query: String,
+        @Query("limit") limit: Int,
+        @Query("offset") offset: Int,
+        @Query("rating") rating: String,
+        @Query("lang") lang: String
+    ): Response<GiphyResponse>
 
 }
